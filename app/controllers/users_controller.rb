13c8c507cfeff9e27972edfef58dda13012f5db7
JakeKViti <thevitij64@gmail.com>
2020-12-16 18:26:03 -0500

    added basic stuff to user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,20 @@
 class UsersController < ApplicationController
+    def new
+        @user = User.new
+    end
+
+    def create
+    end
+
+    def show
+        @user = User.find_by(id: params[:id])
+    end
+
+    private
+    def user_params
+        params.require(:user).permit(
+            :name,
+            :email
+        )
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
   resources :reviews
   resources :games
-  resources :users
+  resources :users #except: [:new]
+
+  #signing up
+  #get '/signup', to: 'users#new'
+
+  #logging in
+  #get '/login', to 'sessions#new'
+  #post '/login', to 'sessions#create'
+  #delete '/login', to 'sessions#destory'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
