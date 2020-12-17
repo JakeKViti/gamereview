373f54cdaf093f5098429fd92ab15485c3ad079c
JakeKViti <thevitij64@gmail.com>
2020-12-16 19:30:21 -0500

    added basic stuff to games controller

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,3 +1,39 @@
 class GamesController < ApplicationController
-
+    def index
+        @games = Game.all
+      end
+    
+      def show
+        @game = Game.find_by(id: params[:id])
+      end
+    
+      def new
+        @game = Game.new
+      end
+    
+      def create
+        game = Game.create(game_params)
+        redirect_to game_path(review)
+      end
+    
+      def edit
+        @game = Game.find_by(id: params[:id])
+      end
+    
+      def update
+        game = Game.find_by(id: params[:id])
+        game.update(game_params)
+        redirect_to game_path(review)
+      end
+    
+      private
+        def game_params
+          params.require(:review).permit(
+            :title,
+            :genre,
+            :developer,
+            :age
+            :released_date
+         )
+    end
 end
