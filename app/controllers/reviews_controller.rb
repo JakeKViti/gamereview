d44eb23749630b01777e75decffe035d826749dc
JakeKViti <thevitij64@gmail.com>
2020-12-16 19:21:15 -0500

    added basic stuff to review controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,37 @@
 class ReviewsController < ApplicationController
+    def index
+        @reviews = Review.all
+      end
+    
+      def show
+        @review = Review.find_by(id: params[:id])
+      end
+    
+      def new
+        @review = Review.new
+      end
+    
+      def create
+        review = Review.create(review_params)
+        redirect_to review_path(review)
+      end
+    
+      def edit
+        @review = Review.find_by(id: params[:id])
+      end
+    
+      def update
+        review = Review.find_by(id: params[:id])
+        review.update(review_params)
+        redirect_to review_path(review)
+      end
+    
+      private
+        def review_params
+          params.require(:review).permit(
+            :title,
+            :body,
+            :rating
+         )
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,7 @@ class UsersController < ApplicationController
     end
 
     def create
+
     end
 
     def show
