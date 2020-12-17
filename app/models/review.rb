738dd35dc9035e5b39446d27cbc810646009930a
JakeKViti <thevitij64@gmail.com>
2020-12-16 19:48:04 -0500

    added some more validators to Review

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,7 +2,7 @@ class Review < ApplicationRecord
     belongs_to :game
     belongs_to :user
 
-    validates :title, presence: true
+    validates :title, presence: true, length: { maximum: 25 }
     validates :body, presence: true
-    validates :rating, presence: true
+    validates :rating, presence: true, numericality: { only_integer: true }
 end
