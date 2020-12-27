ad0b05e0d2c465a5a6a6f9993d277cb22f4662d3
JakeKViti <thevitij64@gmail.com>
2020-12-26 23:36:12 -0500

    Made helpers for review form

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -1,2 +1,17 @@
 module ReviewsHelper
+    def new_or_update?
+        if params[:game_id] != nil
+            return params[:game_id]
+        elsif @review.game_id != nil
+            return @review.game_id
+        end
+    end
+
+    def new_game?
+        if new_or_update? == nil
+           return true
+        end
+    end
+    
+
 end
