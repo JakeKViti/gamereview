49111c402ad86f4c0fb707f8c8dc39d105366d1e
JakeKViti <thevitij64@gmail.com>
2020-12-27 15:57:31 -0500

    Admin have a link in game new and games show that allows them to create and edit games

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -1,2 +1,13 @@
 module GamesHelper
+    def admin_perms_new_game
+        if session[:name] == "Admin"
+            return link_to 'Games New', new_game_path 
+        end
+    end
+
+    def admin_perms_edit_game
+        if session[:name] == "Admin"
+            return link_to 'Games Edit', [:edit, @games] 
+        end
+    end
 end
