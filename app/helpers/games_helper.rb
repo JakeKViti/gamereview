82dbaf82e5f92d0346b74d8a03fb178dd6a62822
JakeKViti <thevitij64@gmail.com>
2020-12-28 00:18:50 -0500

    Adjusted Admin message and got rid of game.title show page text on games index

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -1,13 +1,13 @@
 module GamesHelper
     def admin_perms_new_game
         if session[:name] == "Admin"
-            return link_to 'Games New', new_game_path 
+            return link_to 'Admin, Create A Game Here', new_game_path 
         end
     end
 
     def admin_perms_edit_game
         if session[:name] == "Admin"
-            return link_to 'Games Edit', [:edit, @games] 
+            return link_to 'Admin, Edit This Game Here', [:edit, @games] 
         end
     end
 end
