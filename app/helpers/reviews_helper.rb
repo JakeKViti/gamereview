b6cc9ac892ed73d9357dd21d24dd0cff0f195680
JakeKViti <thevitij64@gmail.com>
2020-12-27 23:59:12 -0500

    redesigned review show, and made it only show link to edit and delete only if you are the owner

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -13,5 +13,15 @@ module ReviewsHelper
         end
     end
     
+    def review_edit_owner?
+        if @review.user_id == session[:user_id]
+            return link_to 'Review Edit', [:edit, @review] 
+        end
+    end
 
+    def review_delete_owner?
+        if @review.user_id == session[:user_id]
+            return button_to "Delete", @review, method: :delete 
+        end
+    end
 end
